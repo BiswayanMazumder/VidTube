4e4f67e2124e5c582d2ff23e1a787321973484a6
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-10-08 12:34:06 +0530

    UI changed

--- src/HomePage/landingpage.jsx
+++ src/HomePage/landingpage.jsx
@@ -208,9 +208,9 @@ export default function Landingpage() {
                 <div className="jjefjdf">
                     <Link>
                         <div className="ejjnejfkd" onClick={() => setSidebar(!sidebar)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" fill='black'>
+                            {/* <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" fill='black'>
                                 <path d="M21 6H3V5h18v1zm0 5H3v1h18v-1zm0 6H3v1h18v-1z"></path>
-                            </svg>
+                            </svg> */}
                         </div>
                     </Link>
                     <Link to={"/"}>
