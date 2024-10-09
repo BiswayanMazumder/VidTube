1361bc509e67111a484b880686ee4c8472c3367d
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-10-09 22:38:46 +0530

    verified logo also added

--- src/User Profiles/profilepage.jsx
+++ src/User Profiles/profilepage.jsx
@@ -158,7 +158,9 @@ export default function ProfilePage() {
                     </div>
                     <div style={{ marginLeft: '10px', marginTop: "20px" }}>
                         <div style={{ fontWeight: "600", fontSize: "22px" }}>
-                            {name}
+                            {name} {
+                                subs.length>0?<svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 0 24 24" width="15" focusable="false" aria-hidden="true"><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zM9.8 17.3l-4.2-4.1L7 11.8l2.8 2.7L17 7.4l1.4 1.4-8.6 8.5z"></path></svg>:<></>
+                            }
                         </div>
                         <div style={{ marginTop: "10px", color: "gray", fontSize: "15px" }}>
                             {
