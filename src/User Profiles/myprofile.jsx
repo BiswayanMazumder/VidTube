746f1c6880a69128d4150f64f575ae34e2cc5f7e
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-10-22 15:19:00 +0530

    bug fixed

--- src/User Profiles/myprofile.jsx
+++ src/User Profiles/myprofile.jsx
@@ -24,11 +24,7 @@ const auth = getAuth(app);
 
 export default function Myprofile() {
     const { userId } = useParams();
-    useEffect(() => {
-        if(userId!==auth.currentUser.uid){
-            window.location.replace('/');
-        }
-    })
+
     const [dp, setDp] = useState('');
     const [name, setName] = useState('');
     const [bio, setBio] = useState('');
